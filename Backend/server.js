c99084e4df16e5b959ae9c28e2def59dba4241b9
Ruturajsinghchouhan <riturajsinghchouhan007@gmail.com>
2026-07-16 20:44:08 +0530

    update server.js cors

--- Backend/server.js
+++ Backend/server.js
@@ -23,7 +23,7 @@ const app = express();
 const server = http.createServer(app);
 const io = new Server(server, {
   cors: {
-    origin: 'http://localhost:5173',
+    origin: 'https://quick-kart-self.vercel.app/user/login',
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
     credentials: true
   }
